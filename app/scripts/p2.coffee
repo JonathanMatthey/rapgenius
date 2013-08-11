631e0df0432c721a61d796e68ba259507b0353c1
manymengofishing <mattheyj@googlemail.com>
2013-08-11 00:35:03 -0400

    added the reorder rows function

--- app/scripts/p2.coffee
+++ app/scripts/p2.coffee
@@ -46,11 +46,16 @@ define [], () ->
 
   clearFlashes= ()->
     $(".flash").removeClass('flash')
+    # $(".activity-module li").each (i,obj) =>
+    #   $(obj).css({
+    #     'position':'relative'
+    #     ,'top':'auto'
+    #     })
 
   getTrendingStats= () ->
     for i in [0..@trendingJSON.length-1]
       if (Math.floor((Math.random()*3)+1) == 1)
-        @trendingJSON[i].song_views += Math.floor((Math.random()*50)+1)
+        @trendingJSON[i].song_views += Math.floor((Math.random()*10000)+1)
       if (Math.floor((Math.random()*10)+1) == 1)
         @trendingJSON[i].annotation_count += Math.floor((Math.random()*10)+1)
       if (Math.floor((Math.random()*20)+1) == 1)
@@ -60,49 +65,64 @@ define [], () ->
           @trendingJSON[i].unreviewed_count += Math.floor((Math.random()*3)+1)
         else
           @trendingJSON[i].unreviewed_count -= Math.floor((Math.random()*3)+1)
+          if @trendingJSON[i].unreviewed_count < 0 then @trendingJSON[i].unreviewed_count = 0
+
+    # sort by views
+    @trendingJSON = _.sortBy @trendingJSON, (stat) ->
+      -stat.song_views
 
     @trendingJSON
 
+  reorderRows= (stats)->
+    $table = $(".activity-module")
+    for i in [0..stats.length-1]
+      $table.find("#"+stats[i].id).css('top',(25 + 55*i) + "px")
+
   refreshStats= (stats) ->
-    $(".activity-module li").each (i,obj) =>
-      if $(obj).attr('id') == stats[i].id
-        if parseInt($(obj).find('.song_views').children('.count').html(),10) != stats[i].song_views
-          $(obj).find('.song_views').children('.count').html(stats[i].song_views)
-          $(obj).find('.song_views').addClass('flash')
-        if parseInt($(obj).find('.annotation_count').children('.count').html(),10) != stats[i].annotation_count
-          $(obj).find('.annotation_count').children('.count').html(stats[i].annotation_count)
-          $(obj).find('.annotation_count').addClass('flash')
-        if parseInt($(obj).find('.verified_count').children('.count').html(),10) != stats[i].verified_count
-          $(obj).find('.verified_count').children('.count').html(stats[i].verified_count)
-          $(obj).find('.verified_count').addClass('flash')
-        if parseInt($(obj).find('.unreviewed_count').children('.count').html(),10) != stats[i].unreviewed_count
-          $(obj).find('.unreviewed_count').children('.count').html(stats[i].unreviewed_count)
-          $(obj).find('.unreviewed_count').addClass('flash')
 
-
+    $table = $(".activity-module")
+    for i in [0..stats.length-1]
+      $row = $table.find("#"+stats[i].id)
+      if parseInt($row.find('.song_views').children('.count').html(),10) != stats[i].song_views
+        $row.find('.song_views').children('.count').html(stats[i].song_views)
+        $row.find('.song_views').addClass('flash')
+      if parseInt($row.find('.annotation_count').children('.count').html(),10) != stats[i].annotation_count
+        $row.find('.annotation_count').children('.count').html(stats[i].annotation_count)
+        $row.find('.annotation_count').addClass('flash')
+      if parseInt($row.find('.verified_count').children('.count').html(),10) != stats[i].verified_count
+        $row.find('.verified_count').children('.count').html(stats[i].verified_count)
+        $row.find('.verified_count').addClass('flash')
+      if parseInt($row.find('.unreviewed_count').children('.count').html(),10) != stats[i].unreviewed_count
+        $row.find('.unreviewed_count').children('.count').html(stats[i].unreviewed_count)
+        $row.find('.unreviewed_count').addClass('flash')
 
   # constructor
   App= () =>
 
     console.log 'p2 ready !'
 
+    @offsets = []
+
+    $(".activity-module li").each (i,obj) =>
+      @offsets.push($(obj).position().top)
+
+    $(".activity-module li").each (i,obj) =>
+      $(obj).css({
+        'position':'absolute'
+        ,'top':@offsets[i] + 'px'
+        })
+
     setInterval( ->
       @stats = getTrendingStats()
+      console.log (_.pluck(@stats, 'song_views'))
+      console.log (_.pluck(@stats, 'id'))
+      reorderRows(@stats)
       refreshStats(@stats)
       setTimeout( ->
         clearFlashes()
       , 1000)
     , 3000)
 
-      # check if item is still in the same order
-
-
-
-    # set timeout every 5 seconds
-
-    # get new trendingJSON
-
-    # assume it comes back ordered
 
 
     # if so, update count
--- app/scripts/p2.js
+++ app/scripts/p2.js
@@ -1,6 +1,6 @@
 (function() {
   define([], function() {
-    var App, clearFlashes, getTrendingStats, refreshStats,
+    var App, clearFlashes, getTrendingStats, refreshStats, reorderRows,
       _this = this;
     this.trendingJSON = [
       {
@@ -48,7 +48,7 @@
       var i, _i, _ref;
       for (i = _i = 0, _ref = this.trendingJSON.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
         if (Math.floor((Math.random() * 3) + 1) === 1) {
-          this.trendingJSON[i].song_views += Math.floor((Math.random() * 50) + 1);
+          this.trendingJSON[i].song_views += Math.floor((Math.random() * 10000) + 1);
         }
         if (Math.floor((Math.random() * 10) + 1) === 1) {
           this.trendingJSON[i].annotation_count += Math.floor((Math.random() * 10) + 1);
@@ -61,38 +61,70 @@
             this.trendingJSON[i].unreviewed_count += Math.floor((Math.random() * 3) + 1);
           } else {
             this.trendingJSON[i].unreviewed_count -= Math.floor((Math.random() * 3) + 1);
+            if (this.trendingJSON[i].unreviewed_count < 0) {
+              this.trendingJSON[i].unreviewed_count = 0;
+            }
           }
         }
       }
+      this.trendingJSON = _.sortBy(this.trendingJSON, function(stat) {
+        return -stat.song_views;
+      });
       return this.trendingJSON;
     };
+    reorderRows = function(stats) {
+      var $table, i, _i, _ref, _results;
+      $table = $(".activity-module");
+      _results = [];
+      for (i = _i = 0, _ref = stats.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+        _results.push($table.find("#" + stats[i].id).css('top', (25 + 55 * i) + "px"));
+      }
+      return _results;
+    };
     refreshStats = function(stats) {
-      var _this = this;
-      return $(".activity-module li").each(function(i, obj) {
-        if ($(obj).attr('id') === stats[i].id) {
-          if (parseInt($(obj).find('.song_views').children('.count').html(), 10) !== stats[i].song_views) {
-            $(obj).find('.song_views').children('.count').html(stats[i].song_views);
-            $(obj).find('.song_views').addClass('flash');
-          }
-          if (parseInt($(obj).find('.annotation_count').children('.count').html(), 10) !== stats[i].annotation_count) {
-            $(obj).find('.annotation_count').children('.count').html(stats[i].annotation_count);
-            $(obj).find('.annotation_count').addClass('flash');
-          }
-          if (parseInt($(obj).find('.verified_count').children('.count').html(), 10) !== stats[i].verified_count) {
-            $(obj).find('.verified_count').children('.count').html(stats[i].verified_count);
-            $(obj).find('.verified_count').addClass('flash');
-          }
-          if (parseInt($(obj).find('.unreviewed_count').children('.count').html(), 10) !== stats[i].unreviewed_count) {
-            $(obj).find('.unreviewed_count').children('.count').html(stats[i].unreviewed_count);
-            return $(obj).find('.unreviewed_count').addClass('flash');
-          }
+      var $row, $table, i, _i, _ref, _results;
+      $table = $(".activity-module");
+      _results = [];
+      for (i = _i = 0, _ref = stats.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+        $row = $table.find("#" + stats[i].id);
+        if (parseInt($row.find('.song_views').children('.count').html(), 10) !== stats[i].song_views) {
+          $row.find('.song_views').children('.count').html(stats[i].song_views);
+          $row.find('.song_views').addClass('flash');
         }
-      });
+        if (parseInt($row.find('.annotation_count').children('.count').html(), 10) !== stats[i].annotation_count) {
+          $row.find('.annotation_count').children('.count').html(stats[i].annotation_count);
+          $row.find('.annotation_count').addClass('flash');
+        }
+        if (parseInt($row.find('.verified_count').children('.count').html(), 10) !== stats[i].verified_count) {
+          $row.find('.verified_count').children('.count').html(stats[i].verified_count);
+          $row.find('.verified_count').addClass('flash');
+        }
+        if (parseInt($row.find('.unreviewed_count').children('.count').html(), 10) !== stats[i].unreviewed_count) {
+          $row.find('.unreviewed_count').children('.count').html(stats[i].unreviewed_count);
+          _results.push($row.find('.unreviewed_count').addClass('flash'));
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
     };
     App = function() {
       console.log('p2 ready !');
+      _this.offsets = [];
+      $(".activity-module li").each(function(i, obj) {
+        return _this.offsets.push($(obj).position().top);
+      });
+      $(".activity-module li").each(function(i, obj) {
+        return $(obj).css({
+          'position': 'absolute',
+          'top': _this.offsets[i] + 'px'
+        });
+      });
       return setInterval(function() {
         this.stats = getTrendingStats();
+        console.log(_.pluck(this.stats, 'song_views'));
+        console.log(_.pluck(this.stats, 'id'));
+        reorderRows(this.stats);
         refreshStats(this.stats);
         return setTimeout(function() {
           return clearFlashes();
